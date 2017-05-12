e1218b9bf88dabd2e88bca41c6ef954aae1328d4
HEdingfield <HEdingfield@users.noreply.github.com>
2017-05-12 09:56:17 -0700

    Fixed syntax, standardized quotes, added TODO

--- howmanyin.py
+++ howmanyin.py
@@ -25,14 +25,15 @@ def search_movie_for_names(url, names):
         names[i] = names[i].lower().capitalize()
 
     r = requests.get(credits_url)
-    soup = BeautifulSoup(r.text, "html.parser")
+    soup = BeautifulSoup(r.text, 'html.parser')
     links = soup.find_all('a')
 
     movie_title = soup.find('a', class_='subnav_heading').string
 
-    # TODO: filter this specifically to the div with id "fullcredits_content"
-    all_names = [i.string.strip() for i in links if i.string != None]
+    # TODO: filter this specifically to the div with id 'fullcredits_content'
+    all_names = [i.string.strip() for i in links if i.string is not None]
 
+    # TODO: possibly rework this with a hash table
     filtered_names = []
     for name in all_names:
         for name_to_match in names:
@@ -46,5 +47,5 @@ def search_movie_for_names(url, names):
 movie_url = 'http://www.imdb.com/title/tt0441773/fullcredits'
 names_to_search = ['Matt', 'Matthew', 'Matti', 'Matty', 'Mat', 'Mathew']
 movie_title, filtered_names = search_movie_for_names(movie_url, names_to_search)
-print(str(movie_title) + " has " + str(len(filtered_names)) + "!!!")
+print(str(movie_title) + ' has ' + str(len(filtered_names)) + '!!!')
 print(filtered_names)
